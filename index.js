59157818fb252ee5605fd03f0278e4daa37b270b
damned <writetodan@yahoo.com>
2017-08-30 15:03:31 +0100

    Wait and carriages info; parsed wait

--- index.js
+++ index.js
@@ -5,11 +5,32 @@ const reqlib = require('request');
 
 
 function Tram($tram) {
-  function parse_destination() {
-    return $tram.find('.departure-destination').text();
+  function field(selector) {
+    return $tram.find(selector).text().trim();
   }
+
+  function parse_carriages() {
+    let carriages = field('.departure-carriages');
+    return carriages;
+  }
+
+  function parse_wait_minutes() {
+    let wait = field('.departure-wait');
+    let parsed_mins = /(\d+)mins?/.exec(wait);
+    if (parsed_mins == null) {
+      return 0;
+    }
+    return parseInt(parsed_mins[1]);
+  }
+
   return {
-    get destination() { return parse_destination(); }
+    get destination() { return field('.departure-destination'); },
+    get carriages() { return parse_carriages(); },
+    get wait() { return {
+        units: 'minutes',
+        duration: parse_wait_minutes()
+      };
+    }
   }
 }
 
@@ -18,10 +39,10 @@ exports.trams = (request, response) => {
   let url = 'https://beta.tfgm.com/public-transport/tram/stops/' + tram_stop_id;
   reqlib.get(url, (error, page_response, body) => {
     let $ = cheerio.load(body);
-    
+
     let trams = $('#departure-items .tram').get().map(el => { return Tram($(el)); });
 
-    response.status(200).send({ trams: trams });
+    response.status(200).send({ stop: { tfgm_id: tram_stop_id }, trams: trams });
   });  
 };
 
