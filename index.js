ff15299a907cba9ae02868bea0591cbcd531f185
damned <writetodan@yahoo.com>
2017-08-30 14:45:11 +0100

    Some live info on gardens' destination names

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict';
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+const cheerio = require('cheerio');
+const reqlib = require('request');
+
+
+function Tram($tram) {
+  function parse_destination() {
+    return $tram.find('.departure-destination').text();
+  }
+  return {
+    get destination() { return parse_destination(); }
+  }
+}
+
+exports.trams = (request, response) => {
+  let tram_stop_id = 'piccadilly-gardens-tram';
+  let url = 'https://beta.tfgm.com/public-transport/tram/stops/' + tram_stop_id;
+  reqlib.get(url, (error, page_response, body) => {
+    let $ = cheerio.load(body);
+    
+    let trams = $('#departure-items .tram').get().map(el => { return Tram($(el)); });
+
+    response.status(200).send({ trams: trams });
+  });  
 };
 
 exports.event = (event, callback) => {
